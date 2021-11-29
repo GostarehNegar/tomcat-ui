bc1e1c646fa32e6c300728c42e1bca83132777f0
pariamahmoudi <paria.mahmoudi@gmail.com>
2021-11-29 06:17:54 -0100

    initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MohsenSellwithGreenCandleCount from "./MohsenSellwithGreenCandleCount";
+import Mohsen from './Mohsen'
+import MohsenSellWithRsi from './MohsenSellWithRSI'
+
+function App() {
+
+  return (
+    <MohsenSellwithGreenCandleCount/>
+  );
+}
+
+export default App;
